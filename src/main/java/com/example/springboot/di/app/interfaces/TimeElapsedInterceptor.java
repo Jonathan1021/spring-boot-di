e3833aef43dbd922de75bc1d3d38f675f256dd93
Jonathan Alberto Vega Ortega <jonathanvo@192.168.1.5>
2023-08-24 11:43:04 -0500

    Interceptor Spring Boot

---
+++ src/main/java/com/example/springboot/di/app/interfaces/TimeElapsedInterceptor.java
@@ -0,0 +1,41 @@
+package com.example.springboot.di.app.interfaces;
+
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component("TimeElapsedInterceptor")
+public class TimeElapsedInterceptor implements HandlerInterceptor {
+	
+	private static final Logger logger = LoggerFactory.getLogger(TimeElapsedInterceptor.class);
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		long timeStart = System.currentTimeMillis();
+		logger.info("TimeElapsedInterceptor: PreHandler Start...");
+		request.setAttribute("TimeStart", timeStart);
+		
+		Random random = new Random();
+		Integer demora = random.nextInt(500);
+		Thread.sleep(demora);
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		long timeEnd = System.currentTimeMillis();
+		long timeStart = (long) request.getAttribute("TimeStart");
+		long timeTotal = timeEnd - timeStart;
+		logger.info("TimeElapsedInterceptor: PostHandle End... " + timeTotal + "ms");
+	}
+	
+	
+
+}
